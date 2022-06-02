f4e77a68591d1257288066960ac4dcd3c5fab69f
faenerisis <67673597+faenerisis@users.noreply.github.com>
2022-06-02 18:22:17 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,7 +8,7 @@ import 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyC_P9sOM4Sxi1Z4ds0QzFAornu2tj4gieE',
+  //apiKey: 'Your apikey should be different so please insert your apikey here',
   authDomain: 'internf-auth.firebaseapp.com',
   projectId: 'internf-auth',
   storageBucket: 'internf-auth.appspot.com',
